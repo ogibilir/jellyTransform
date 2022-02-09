ef96dc780901873feaaab00cccfd88a1cc2b5d11
ogibilir <78707796+ogibilir@users.noreply.github.com>
2022-02-09 13:08:38 +0300

    Change level, Add mechanics, Create Prefab
    Level changed. Freeze mechanic active now.

--- Assets/Scripts/TransformManager.cs
+++ Assets/Scripts/TransformManager.cs
@@ -16,6 +16,12 @@ public class TransformManager : MonoBehaviour
     [SerializeField] private List<GameObject> _star;
     [SerializeField] private List<GameObject> _snow;
 
+    [SerializeField] private List<GameObject> _lastParticle;
+
+    [SerializeField] private ParticleSystem _snowParticle;
+    [SerializeField] private ParticleSystem _heartParticle;
+    [SerializeField] private ParticleSystem _starParticle;
+
 
     public ParticleSystem particle;
 
@@ -26,7 +32,26 @@ public class TransformManager : MonoBehaviour
         #region Kaybetme
         if (other.tag == "Saw" || other.tag == "Spikes")
         {
+            if(_lastParticle.Count != 0)
+            {
+                StartCoroutine(StopParticle(_lastParticle[0].name));
+            }
+            StartCoroutine(DisableCamera());
+            transform.DOJump(new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z - 2),3f,1,1f);
+            if(other.tag == "Spikes")
+            {
+                Destroy(other.gameObject.GetComponent<Collider>());
+                other.transform.DOMoveY(-1f, 2f);
+            }   
+            if (_lastParticle.Count != 0)
+            {
+                for(int i = 0; i < _lastParticle.Count; i++)
+                {
 
+                    _lastParticle[i].SetActive(false);
+                }
+            }
+            _lastParticle.Clear();
         }
         if(other.tag == "Space")
         {
@@ -37,12 +62,13 @@ public class TransformManager : MonoBehaviour
             Vector3 currentPos = transform.position;
             Vector3 diePos = new Vector3(transform.position.x, -120f, transform.position.z);
             Debug.Log("Oyunu bitir");
-            transform.DOMoveY(-6f, 2f).SetEase(Ease.InOutElastic);
+            transform.DOMoveY(-6f, 1.5f).SetEase(Ease.InOutElastic);
         }
         #endregion
         #region Sekil Degistirme
         if (other.gameObject.tag == "Comb")
         {
+            StartCoroutine(DisableCamera());
             PlayerMovement._stopTime = 2f;
             if (gameObject.layer == 8)
             {
@@ -58,6 +84,7 @@ public class TransformManager : MonoBehaviour
         }
         if (other.tag == "Cube")
         {
+            StartCoroutine(DisableCamera());
             PlayerMovement._stopTime = 2f;
             if (gameObject.layer == 10)
             {
@@ -73,6 +100,7 @@ public class TransformManager : MonoBehaviour
         }
         if (other.tag == "Clasp")
         {
+            StartCoroutine(DisableCamera());
             PlayerMovement._stopTime = 2f;
             if (gameObject.layer == 8)
             {
@@ -90,23 +118,38 @@ public class TransformManager : MonoBehaviour
         #region Dokme islemi
         if (other.tag == "Star")
         {
+            if (_lastParticle.Count != 0)
+            {
+                _lastParticle.Clear();
+            }
             for (int i = 0; i < _star.Count; i++)
             {
                 _star[i].SetActive(true);
-            }
+                _lastParticle.Add(_star[i]);
+            }            
         }
         if (other.tag == "Heart")
         {
+            if (_lastParticle.Count != 0)
+            {
+                _lastParticle.Clear();
+            }
             for (int i = 0; i < _heart.Count; i++)
             {
                 _heart[i].SetActive(true);
-            }
+                _lastParticle.Add(_heart[i]);
+            }           
         }
         if(other.tag == "Snow")
         {
-            for (int i = 0; i < _heart.Count; i++)
+            if (_lastParticle.Count != 0)
+            {
+                _lastParticle.Clear();
+            }
+            for (int i = 0; i < _snow.Count; i++)
             {
                 _snow[i].SetActive(true);
+                _lastParticle.Add(_snow[i]);
             }
         }
         #endregion
@@ -124,5 +167,38 @@ public class TransformManager : MonoBehaviour
             _pieRenderer.GetComponent<Renderer>().material = blue;
         }
         #endregion
+        #region Bitis
+        if(other.tag == "Freeze")
+        {
+            PlayerMovement._stopTime = 2f;
+            StartCoroutine(DisableCamera());
+        }
+        #endregion
+    }
+    public IEnumerator DisableCamera()
+    {
+        Camera.main.gameObject.GetComponent<CameraScript>().enabled = false;
+        yield return new WaitForSeconds(1f);
+        Camera.main.gameObject.GetComponent<CameraScript>().enabled = true;
+    }
+    public IEnumerator StopParticle(string lastparticle)
+    {
+        if(lastparticle == "Heart")
+        {
+            _heartParticle.gameObject.SetActive(true);
+        }
+        if(lastparticle == "Snow")
+        {
+            _snowParticle.gameObject.SetActive(true);
+        }
+        if(lastparticle == "Star")
+        {
+            _starParticle.gameObject.SetActive(true);
+
+        }
+        yield return new WaitForSeconds(1f);
+        _snowParticle.gameObject.SetActive(false);
+        _heartParticle.gameObject.SetActive(false);
+        _starParticle.gameObject.SetActive(false);
     }
 }
