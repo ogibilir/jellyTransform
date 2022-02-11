b2d2abeeb1d7f905330f267b81fcdc600db458d8
AsliAsln <asliaslan450@gmail.com>
2022-02-11 19:26:20 +0300

    - delete : Scripts
    scripts unrelated to the game have been deleted

--- Assets/Scripts/Data_Object_Class.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data_object", menuName = "Examples/")]
-public class Data_Object_Class : ScriptableObject
-{
-    [SerializeField] public int max_reachable_level = 0;
-}
